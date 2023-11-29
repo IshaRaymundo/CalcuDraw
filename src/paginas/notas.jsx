5877e816463957ac5e1c53f14f28b02bc36f5fd6
DiegoAle6 <diegoalemanmena@gmail.com>
2023-11-28 23:52:29 -0500

    a

--- src/paginas/notas.jsx
+++ src/paginas/notas.jsx
@@ -20,6 +20,12 @@ function Notas() {
   };
 
   const saveNote = () => {
+    // Verificar que la nota no esté en blanco antes de guardar
+    if (note.trim() === '') {
+      // Puedes mostrar un mensaje de error o simplemente no hacer nada
+      return;
+    }
+
     // Si hay una nota seleccionada, actualiza esa nota; de lo contrario, guarda una nueva nota
     if (selectedNoteIndex !== null) {
       const updatedNoteHistory = [...noteHistory];
@@ -51,6 +57,21 @@ function Notas() {
     setSelectedNoteIndex(index);
   };
 
+  const downloadNotes = () => {
+    const notesText = noteHistory.join('\n');
+    const blob = new Blob([notesText], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'notas.txt';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
+  
+  // ... (código posterior)
+  
   return (
     <div className="bg-[#f7f7f8] min-h-screen">
       <header className="bg-blue-900 text-white p-4">
@@ -58,32 +79,31 @@ function Notas() {
       </header>
       <div className="container mx-auto mt-5">
         <div className="flex">
+        <div className="w-1/2 p-4">
+  <div className="calculator rounded-md bg-yellow-200 p-4 shadow">
+    <textarea
+      value={note}
+      onChange={(e) => setNote(e.target.value)}
+      className="form-control h-64 bg-white"
+      placeholder="Toma tus notas aquí"
+    />
+    <div className="mt-3 d-flex justify-content-between">
+      <button onClick={saveNote} className="btn btn-light">
+        Guardar
+      </button>
+      <button onClick={clearNote} className="btn btn-light">
+        Limpiar
+      </button>
+      <button onClick={downloadNotes} className="btn btn-light">
+        Descargar
+      </button>
+    </div>
+  </div>
+</div>
           <div className="w-1/2 p-4">
-            <div className="calculator rounded-md bg-blue-200 p-4">
-              <textarea
-                value={note}
-                onChange={(e) => setNote(e.target.value)}
-                className="form-control h-64 bg-white"
-                placeholder="Toma tus notas aquí"
-              />
-              <button
-                onClick={clearNote}
-                className="btn btn-primary mt-2"
-              >
-                Limpiar Notas
-              </button>
-              <button
-                onClick={saveNote}
-                className="btn btn-primary mt-2"
-              >
-                {selectedNoteIndex !== null ? 'Actualizar Nota' : 'Guardar Nota'}
-              </button>
-            </div>
-          </div>
-          <div className="w-1/2 p-4">
-            <div className="notes bg-yellow-200 rounded-md p-4">
+          <div className="notes bg-white rounded-md p-4 shadow">
               <h2 className="text-2xl font-semibold mb-4">Historial de Notas</h2>
-              <table className="table">
+              <table className="table ">
                 <thead>
                   <tr>
                     <th>Nota</th>
@@ -94,16 +114,12 @@ function Notas() {
                   {noteHistory.map((savedNote, index) => (
                     <tr key={index}>
                       <td>{savedNote}</td>
-                      <td className="text-center">
-                        <button
-                          onClick={() => editNote(index)}
-                        >
-                          <FaEdit color="#ffc107" /> 
+                      <td className="text-center space-x-2">
+                        <button onClick={() => editNote(index)}>
+                          <FaEdit color="#ffc107" />
                         </button>
-                        <button
-                          onClick={() => deleteNote(index)}
-                        >
-                          <FaTrash color="#dc3545" /> 
+                        <button onClick={() => deleteNote(index)}>
+                          <FaTrash color="#dc3545" />
                         </button>
                       </td>
                     </tr>
